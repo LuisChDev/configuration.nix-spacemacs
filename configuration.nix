d3abe5d113fdb27516914dc2dc7a2cf582c2b6ef
Luis Chavarriaga <luischa123@gmail.com>
2020-02-23 20:51:22 -0500

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,149 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  myPythonPkgs = python-packages: with python-packages; [
+    python-language-server
+  ];
+  pythonWithPkgs = pkgs.python3.withPackages myPythonPkgs;
+in
+  {
+    imports =
+      [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+      ];
+
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.loader.grub.extraEntries = ''
+  menuentry "Ubuntu" {
+    set root=(hd0, 2)
+    chainloader /EFI/ubuntu/grubx64.efi
+  	}
+  '';
+
+    networking.hostName = "nixos";  # Define your hostname.
+    networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.wicd.enable = true;
+
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    networking.useDHCP = false;
+    networking.interfaces.docker0.useDHCP = true;
+    networking.interfaces.enp2s0.useDHCP = true;
+    networking.interfaces.wlp3s0.useDHCP = true;
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Select internationalisation properties.
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+    };
+
+    console.font = "Lat2-Terminus16";
+    console.useXkbConfig = true;
+
+    # Set your time zone.
+    time.timeZone = "America/Bogota";
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      # text readers & editors
+      vim
+      emacs
+      evince
+
+      # internet
+      firefox
+      plasma-browser-integration
+
+      # languages
+      pythonWithPkgs
+
+      # system
+      gparted
+      ecryptfs
+      ecryptfs-helper
+      gnome3.gnome-disk-utility
+      filelight
+
+      # utilities
+      killall
+      wget
+      pass
+      powertop
+      git
+      tree
+
+      # crap
+      kdeFrameworks.oxygen-icons5
+    ];
+
+    # start powertop on startup
+    powerManagement.powertop.enable = true;
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    programs.mtr.enable = true;
+    programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    networking.firewall.allowedTCPPorts = [ 22 53 80 443 8000 6667 6697 ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    # enable Docker containers.
+    virtualisation.docker.enable = true;
+
+    # enable emacs as a daemon.
+    services.emacs.enable = true;
+    services.emacs.defaultEditor = true;
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+    services.xserver.layout = "latam,ru,gr";
+    services.xserver.xkbOptions = "eurosign:e";
+
+    # Enable touchpad support.
+    services.xserver.libinput.enable = true;
+
+    # Enable the KDE Desktop Environment.
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.chava = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
+      hashedPassword = "***REMOVED***";
+      home = "/home/chava/";
+
+    };
+
+    # This value determines the NixOS release with which your system is to be
+    # compatible, in order to avoid breaking some software such as database
+    # servers. You should change this only after NixOS release notes say you
+    # should.
+    system.stateVersion = "19.09"; # Did you read the comment?
+  }
