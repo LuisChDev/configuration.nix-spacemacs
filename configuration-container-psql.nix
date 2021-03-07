b2a0911ec196ff989e46ad47bae0939a0c589ca8
Luis Chavarriaga <luischa123@gmail.com>
2021-03-06 21:50:25 -0500

    add configuration for container
    el archivo incluido en este commit puede usarse para configurar
    un container nixOS con una base de datos PostgreSQL.

---
+++ configuration-container-psql.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{ boot.isContainer = true;
+  networking = {
+    hostName = mkDefault "colvid-db";
+    useDHCP = false;
+    firewall.allowedTCPPorts = [ 22 5432 ];
+  };
+
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoWa4CmI4If9iDHSbQAST1jEkqHfRoJIT+j2pZG6aPgrLMvl1KOzuIr6rkVSUS12XYFMxE8lD7GH9IOjIaRHTa3eDOSVKp2if0Kkd70q8dZyOJ7PWquGxbihvH8yngtOBr9rHG/gsBwdDqMbFV3Ye4dNYohby5xfXe9/6K45j8eNuT4A3CXrT9eeObc/22It8phCLSl54EgqmreJTE9QsD8BsiEcaEIMOgEiCjyH7v/k1xVCkhIBCLNaXi65DAlVFmT2QDvJ4HpGvg/KeFKXkWNDpnz2iZP54KWASP0EStc8v2b2mP5GRlWqj7BkAaNJ3HNqg/6QhP9FVZUxROs66j chava@phoenix"];
+
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    package = pkgs.postgresql;
+
+    authentication = ''
+      host  all  all  10.0.0.0/8  trust
+    '';
+
+    ensureUsers = [ 
+      {
+	name = "chava";
+	ensurePermissions = { 
+          "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES"; 
+	};
+      }
+      {
+        name = "root";
+	ensurePermissions = {
+	  "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+	};
+      }
+    ];
+  };
+  
+}
